3d0e239c0539fff26f2c27b676407d0e52e7388b
Andre Guzman <an.guzmn@gmail.com>
2025-12-22 23:13:03 -0500

    Updates to personal keymaps. Replace pipe with backslash (for shift support). Swap space for backspace for right thumb key.

--- keyboards/lily58/keymaps/andreguzman/keymap.c
+++ keyboards/lily58/keymaps/andreguzman/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,       KC_Q,   KC_W,       KC_E,       KC_R,       KC_T,                               KC_Y,       KC_U,       KC_I,    KC_O,    KC_P,     KC_ENT,
   KC_LSFT,      KC_A,   KC_S,       KC_D,       KC_F,       KC_G,                               KC_H,       KC_J,       KC_K,    KC_L,    KC_SCLN,  KC_RSFT,
   KC_LCTRL,     KC_Z,   KC_X,       KC_C,       KC_V,       KC_B,       KC_LBRC,    KC_RBRC,    KC_N,       KC_M,       KC_COMM, KC_DOT,  KC_SLSH,  KC_RCTRL,
-                                    KC_LGUI,    KC_RALT,    MO(_LOWER), KC_SPC,     KC_SPC,     MO(_RAISE), KC_LALT,    KC_RGUI
+                                    KC_LGUI,    KC_RALT,    MO(_LOWER), KC_SPC,     KC_BSPC,     MO(_RAISE), KC_LALT,    KC_RGUI
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT(
   KC_GRV,   KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,                               KC_F6,     KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_DEL,
   _______,  _______,    _______,    _______,    _______,    _______,                            _______,    _______,    _______,    _______,    _______,    _______,
-  _______,  KC_EXLM,    KC_AT,      KC_HASH,    KC_DLR,     KC_PERC,                            KC_CIRC,    KC_MINS,    KC_EQL,     KC_PIPE,    KC_QUOT,    _______,
+  _______,  KC_EXLM,    KC_AT,      KC_HASH,    KC_DLR,     KC_PERC,                            KC_CIRC,    KC_MINS,    KC_EQL,     KC_BSLS,    KC_QUOT,    _______,
   _______,  _______,    _______,    _______,    _______,    _______,    KC_F11,     KC_F12,     _______,    KC_AMPR,    KC_PLUS,    KC_LBRC,    KC_RBRC,    _______,
                                     _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______
 ),
